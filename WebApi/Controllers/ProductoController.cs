28f3281890b61b8568a4b35c34fb95f03b06457d
Gonzalo <gonzalogomez22@gmail.com>
2021-08-03 20:54:27 -0300

    Configuracion MarketDbContext, MarketDbContextData, impl ProductoRepository, ProductoController, Migracion inicial

---
+++ WebApi/Controllers/ProductoController.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private readonly IProductoRepository productoRepository;
+
+        public ProductoController(IProductoRepository productoRepository)
+        {
+            this.productoRepository = productoRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Producto>>> GetProductos()
+        {
+            var productos = await productoRepository.GetProductosAsync();
+            return Ok(productos);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Producto>> GetProducto(int id)
+        {
+            return await productoRepository.GetProductoByIdAsync(id);
+        }
+
+    }
+}
